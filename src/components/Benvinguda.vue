95e52cb8698d8ccf8b69650911036ec0cbc9e249
Ana María Águila <ana_mari_aguila@hotmail.com>
2023-01-16 16:40:25 +0100

    Ej5. Pantalla de bienvenida

---
+++ src/components/Benvinguda.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="inici">
+        <h1>Gestió Empresarial VUE</h1>
+        <p>Benvingut/da al nostre tutorial per a nous usuaris</p>
+        <p>A continuació et mostrem un petit tutorial. Pots navegar pels botons per accedir als diferents consells.</p>
+
+        <button @click="$emit('cambia',1)">Iniciar</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Benvinguda',
+        emits: ['cambia']
+    }
+</script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,32 @@
 <template>
-    <Botons @accio="accionBtn"></Botons>
-    <Escena :frases="frases" :currentSentence="currentSentence"></Escena>   
+    <Benvinguda @cambia="cambiaInici" v-if="!btnInici"></Benvinguda>
+    
+    <div v-if="btnInici">   
+        <Botons @accio="accionBtn"></Botons>
+        <Escena :frases="frases" :currentSentence="currentSentence"></Escena> 
+    </div>
+
+
 </template>
 
 <script>
+    import Benvinguda from './Benvinguda.vue'
     import Botons from './Botons.vue'
     import Escena from './Escena.vue'
 
     export default {
         name: 'Home',
         components: {
+            Benvinguda,
             Botons,
             Escena
         },
         methods: {
+            cambiaInici(posicion){
+              this.btnInici = posicion; 
+              console.log(this.btnInici);
+              return this.btnInici; 
+            },
             accionBtn(event){
                 if(!event){
                     if(this.currentSentence>0){
@@ -32,6 +45,7 @@
         },
         data() {
             return {
+                btnInici: 0,
                 currentSentence: 0,
                 frases: [
                 "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial",
