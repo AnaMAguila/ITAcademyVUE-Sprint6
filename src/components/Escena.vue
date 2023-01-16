5fadb66c0df47233d10ce59214f3181059f17ac4
Ana María Águila <ana_mari_aguila@hotmail.com>
2023-01-16 16:18:49 +0100

    Ej2. Leer de prop o de archivo json

--- src/components/Escena.vue
+++ src/components/Escena.vue
@@ -1,11 +1,35 @@
 <template>
     <div>
-        El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial
+        <!-- MUESTRA LOS DATOS DEL DATA HOME.VUE -->
+        <ul>
+            <li v-for="text in frases" :key="text">
+                {{ text }}
+            </li>
+        </ul>
+
+        <!-- MUESTRA LOS DATOS DEL ARCHIVO JSON  -->            
+        <!-- <ul>
+            <li v-for="text in obtenFrases" :key="text">
+                {{ text }}
+            </li>
+        </ul> -->
+   
     </div>
 </template>
 
 <script>
+    // IMPORTA LOS DATOS DEL ARCHIVO JSON  
+    // import arrayFrases from '../data/frases.json';
+
     export default {
-    name: 'Escena'
+        name: 'Escena',
+        props: ['frases']
+        // MUESTRA LOS DATOS DEL ARCHIVO JSON  
+        // computed: {
+        //     // método donde obtengo el array de frases con map
+        //     obtenFrases() {
+        //         return arrayFrases.map((item) => item)
+        //     }
+        // }
     }
 </script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-    <Escena></Escena>
+    <Escena :frases="frases"></Escena>  
 </template>
 
 <script>
@@ -9,6 +9,16 @@
         name: 'Home',
         components: {
             Escena
+        },
+        data() {
+            return {
+                frases: [
+                "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial",
+                "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.",
+                "L'heroi va decidir travessar la porta que el portava a casa",
+                "Mentrestant, altres herois no van tenir tanta sort en la seva elecció ..."
+                ]
+            }            
         }
     }
 </script>
