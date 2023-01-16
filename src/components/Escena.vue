532e53f972360ab03ab8388df84cf6b7c99ae3d8
Ana María Águila <ana_mari_aguila@hotmail.com>
2023-01-16 16:11:39 +0100

    Ej1. Creación componentes y mostrar línea

---
+++ src/components/Escena.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial
+    </div>
+</template>
+
+<script>
+    export default {
+    name: 'Escena'
+    }
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <Escena></Escena>
+</template>
+
+<script>
+    import Escena from './Escena.vue'
+
+    export default {
+        name: 'Home',
+        components: {
+            Escena
+        }
+    }
+</script>
